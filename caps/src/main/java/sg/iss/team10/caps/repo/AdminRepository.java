537ec8d2b34cbf917230d247d755a88f2e51fcb6
fiona <fionatqh@yahoo.com>
2018-12-11 11:54:21 +0800

    admin repo query - findAllAdmin

--- caps/src/main/java/sg/iss/team10/caps/repo/AdminRepository.java
+++ caps/src/main/java/sg/iss/team10/caps/repo/AdminRepository.java
@@ -1,9 +1,13 @@
 package sg.iss.team10.caps.repo;
 
+import java.util.ArrayList;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import sg.iss.team10.caps.model.Admin;
 
 public interface AdminRepository extends JpaRepository<Admin, Integer> {
-
+@Query("SELECT a FROM Admin a")
+ArrayList<Admin> findAllAdmin();
 }
