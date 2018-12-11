f3665b2bc7c51c6ae2078b55155af88c58b2d183
hp <hp@172.17.100.134>
2018-12-11 14:25:47 +0800

    admin service and impl

--- caps/src/main/java/sg/iss/team10/caps/services/AdminService.java
+++ caps/src/main/java/sg/iss/team10/caps/services/AdminService.java
@@ -1,5 +1,15 @@
 package sg.iss.team10.caps.services;
 
-public interface AdminService {
+import sg.iss.team10.caps.repo.AdminRepository;
+import sg.iss.team10.caps.model.Admin;
+import java.util.ArrayList;
 
+public interface AdminService {
+	
+	Admin removeAdmin(Admin admin);
+	Admin changeAdmin(Admin admin);
+	Admin createAdmin(Admin admin);
+	ArrayList<Admin> findAllAdmin();
+	Admin findAdminByName(String name);
+	Admin findAdminById(Integer id);
 }
--- caps/src/main/java/sg/iss/team10/caps/services/AdminServiceImpl.java
+++ caps/src/main/java/sg/iss/team10/caps/services/AdminServiceImpl.java
@@ -1,5 +1,60 @@
 package sg.iss.team10.caps.services;
 
+import sg.iss.team10.caps.repo.AdminRepository;
+import sg.iss.team10.caps.model.Admin;
+import java.util.ArrayList;
+import javax.annotation.Resource;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
 public class AdminServiceImpl implements AdminService {
+@Resource
+private AdminRepository adminRepository;
+
+@Override
+@Transactional
+public Admin findAdminById(Integer id)
+{
+	return adminRepository.findAdminById(id);
+}
+
+@Override
+@Transactional
+public Admin findAdminByName(String name)
+{
+	return adminRepository.findAdminByName(name);
+}
+
+@Override
+@Transactional
+public ArrayList<Admin> findAllAdmin()
+{
+	ArrayList<Admin> findall= (ArrayList<Admin>)adminRepository.findAllAdmin();
+	return findall;
+}
+
+@Override
+@Transactional
+public Admin createAdmin(Admin admin)
+{
+	return AdminRepository.saveAndFlush(admin);
+}
+
+@Override
+@Transactional
+public Admin changeAdmin(Admin admin)
+{
+	return AdminRepository.saveAndFlush(admin);
+}
+
+@Override
+@Transactional
+public void removeAdmin(Admin admin)
+{
+	AdminRepository.delete(admin);
+}
+
+
 
 }
