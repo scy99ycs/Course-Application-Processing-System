51de1173d665ab1948edcf5d8363dfb3b5117987
Priyanka <Priyanka@172.17.14.11>
2018-12-11 12:26:25 +0800

    repo admin

--- caps/src/main/java/sg/iss/team10/caps/repo/AdminRepository.java
+++ caps/src/main/java/sg/iss/team10/caps/repo/AdminRepository.java
@@ -7,7 +7,15 @@ import org.springframework.data.jpa.repository.Query;
 
 import sg.iss.team10.caps.model.Admin;
 
-public interface AdminRepository extends JpaRepository<Admin, Integer> {
+public interface AdminRepository extends JpaRepository<Admin, Integer> 
+{
+	@Query("SELECT a from Admin a where a.adminId =:id")
+	Admin findAdminById(@Param("id") int id);
+	
+	@Query("SELECT a FROM a.Admin WHERE a.adminName=:name")
+	Admin findAdminByName(@Param("name") String name);
+
+
 @Query("SELECT a FROM Admin a")
 ArrayList<Admin> findAllAdmin();
 }
