e7a0a2cab06f49d1da5c2449e6150f911c447aaf
Kyler Lee <44352302+kylerlee@users.noreply.github.com>
2018-12-11 13:45:38 +0800

    Kyler commit AdminRepository

--- caps/src/main/java/sg/iss/team10/caps/repo/AdminRepository.java
+++ caps/src/main/java/sg/iss/team10/caps/repo/AdminRepository.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import sg.iss.team10.caps.model.Admin;
 
