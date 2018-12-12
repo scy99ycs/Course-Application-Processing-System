8039698007786beec6d80f38b45519d1beeb36fa
hp <hp@172.17.100.134>
2018-12-12 10:23:42 +0800

    admin student(controller & list jsp)

--- caps/src/main/java/sg/iss/team10/caps/controllers/AdminStudentController.java
+++ caps/src/main/java/sg/iss/team10/caps/controllers/AdminStudentController.java
@@ -1,6 +1,97 @@
 package sg.iss.team10.caps.controllers;
 
-public class AdminStudentController {
+import java.util.List;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import sg.iss.team10.caps.model.Student;
+import sg.iss.team10.caps.services.StudentService;
+
+@RequestMapping(value="/admin/student")
+@Controller
+public class AdminStudentController {
 	
+	@Autowired
+	private StudentService sService;
+	
+	@RequestMapping(value="/add",method=RequestMethod.GET)
+	public ModelAndView newAddStudentPage()
+	{
+		ModelAndView mav= new ModelAndView("student-new","student",new Student());
+		mav.addObject("sidlist", sService.findAllStudents());
+		return mav;
+	}
+	
+	@RequestMapping(value = "/add", method = RequestMethod.POST)
+	public ModelAndView createNewStudent(@ModelAttribute @Valid Student student, BindingResult result,
+			final RedirectAttributes redirectAttributes) {
+
+		if (result.hasErrors())
+			return new ModelAndView("student-new");
+
+		ModelAndView mav = new ModelAndView();
+		String message = "New Student " + student.getStudentId() + " was successfully created.";
+
+		sService.createStudent(student);
+		mav.setViewName("redirect:/admin/student/list");
+
+		redirectAttributes.addFlashAttribute("message", message);
+		return mav;
+	}
+	
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	public ModelAndView studentListPage() {
+		ModelAndView mav = new ModelAndView("AdminStudentList");
+		List<Student> studentList = sService.findAllStudents();
+		mav.addObject("studentList", studentList);
+		return mav;
+	}
+	
+	@RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
+	public ModelAndView editStudentPage(@PathVariable int id) {
+		ModelAndView mav = new ModelAndView("student-edit");
+		Student student = sService.findStudentByStudentID(id);
+		mav.addObject("student", student);
+		mav.addObject("sidlist", sService.findAllStudents());
+		return mav;
+	}
+
+	@RequestMapping(value = "/edit/{id}", method = RequestMethod.POST)
+	public ModelAndView editStudent(@ModelAttribute @Valid Student student, BindingResult result,
+			@PathVariable String id, final RedirectAttributes redirectAttributes) /*throws DepartmentNotFound*/ {
+
+		if (result.hasErrors())
+			return new ModelAndView("student-edit");
+
+		ModelAndView mav = new ModelAndView("redirect:/admin/student/list");
+		String message = "Student was successfully updated.";
+
+		sService.updateStudent(student);
+
+		redirectAttributes.addFlashAttribute("message", message);
+		return mav;
+	}
+		
+	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+	public ModelAndView deleteStudent(@PathVariable int id, final RedirectAttributes redirectAttributes)
+			/*throws DepartmentNotFound*/ {
+
+		ModelAndView mav = new ModelAndView("redirect:/admin/student/list");
+		Student student = sService.findStudentByStudentID(id);
+		sService.removeStudent(student);
+		String message = "The student " + student.getStudentId() + " was successfully deleted.";
+
+		redirectAttributes.addFlashAttribute("message", message);
+		return mav;
+	}
 }
---
+++ caps/src/main/webapp/WEB-INF/views/AdminStudentList.jsp
@@ -0,0 +1,65 @@
+<%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ taglib uri="http://www.springframework.org/tags" prefix="spring"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/functions" prefix="fn"%>
+<%@ taglib uri="http://www.springframework.org/tags/form" prefix="form"%>
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+    
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<form:form>
+<c:if test="${fn:length(studentList) gt 0}">
+<table>
+	<tr>
+		<th><spring:message code="Student Id"/></th>
+		<th><spring:message code="First & Middle Name"/></th>
+		<th><spring:message code="Last Name"/></th>
+		<th><spring:message code="User Name"/></th>
+		<th><spring:message code="Edit"/></th>
+		<th><spring:message code="Delete"/></th>
+	</tr>
+	
+	<c:forEach var="student" items="${studentList}">
+	<tr>
+		<td>${student.studentId}</td>
+		<td>${student.firstMidName}</td>
+		<td>${student.lastName}</td>
+		<td>${student.username}</td>
+		<td align="center"><a
+				href="${pageContext.request.contextPath}/admin/student/edit/${student.studentId}.html"><spring:message
+						code="Edit" /></a></td>
+		<td><a
+				href="${pageContext.request.contextPath}/admin/student/delete/${student.studentId}.html"><spring:message
+						code="Delete" /></a></td>
+	</tr>
+	</c:forEach>
+</table>
+</c:if>	
+</form:form>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
